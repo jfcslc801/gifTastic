7a993d301b90d6fdc553f753600c2da5b7503504
jaime franco cerritos <jfcslc@outlook.com>
2018-03-02 12:55:06 -0700

    readMe update

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# gifTastic
+# GifTastic
+University of Utah Coding Bootcamp.  
+- Week 6 Assignment: GifTastic!
+
+---
+In this assignment, we used a GIPHY API to make a dynamic web page that populates with gifs of your choice. To complete this task,  I called the GIPHY API, used JavaScript and jQuery to change the HTML of your site.
+
+## Getting Started 
+Play here: https://jfcslc801.github.io/gifTastic/
+
+## Screenshots
+Press start to begin!
+![Screenshot](gifTastic.png)
+![Demo] (https://jfcslc801.github.io/gifTastic/)
+
+## Technologies used
+- Bootstrap
+- jQuery
+- CSS
+- GIPHY API
+
+### Prerequisites
+
+```
+- Bootstrap: visit http://getbootstrap.com/getting-started/ for the CDN and link in html
+- jQuery: visit http://code.jquery.com/ and link in html
+```
+
+## Built With
+
+* VS - Visual Code Studio - Text Editor
+* Bootstrap- Wireframe
+* GIPHY API visit https://developers.giphy.com/
+
+## Authors
+* **Jaime Franco Cerritos** - *HTML/CSS/JS* - [Jaime Franco Cerritos](https://github.com/jfcslc801)
+
+## Acknowledgments
+
+* Thanks to **University of Utah Coding Bootcamp** for additional help.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,72 @@
+//VAR topics used to store strings search topics.........................................................................
 var topics = ["The Little Mermaid", "Beauty and the Beast", "Aladdin", "Tangled", "Pocahontas", "Snow White", "Pinocchio", "Dumbo ", "Bambi", "Cinderella", "Peter Pan", "Alice in Wonderland","Sleeping Beauy", "The Jungle Book"];
 
+
+//jquery function runs upon page loading
+//search array 'topics', 
+//adds class 'searchButton' to every button, 
+//ID '#buttonsArea' where buttons will generate on html.
 $(function () {
     populateButtons(topics, 'searchButton', '#buttonsArea');
 })
 
+//...............................................
+//Function Populates '#buttonsArea' in html with buttons...
 function populateButtons(topics, classToAdd, areaToAddTo) {
-    $(areaToAddTo).empty();
+    $(areaToAddTo).empty();                                  //clears button area to avoid adding duplicates
     for (var i = 0; i < topics.length; i++) {
-        var a = $('<button>');
-        a.addClass(classToAdd);
-        a.attr('data-type', topics[i]);
-        a.text(topics[i]);
-        $(areaToAddTo).append(a);
+        var btn = $('<button>');
+        btn.addClass(classToAdd);
+        btn.attr('data-type', topics[i]);
+        btn.text(topics[i]);
+        $(areaToAddTo).append(btn);
     }
 }
 
+// ......................................................
+//  reference data-type using jquery
+//
 $(document).on('click', '.searchButton', function () {
     $('#searches').empty();
     var type = $(this).data('type');
+    //API KEY
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         type + "&api_key=W0R1kQZTbvtUYYhybCKuin2QvCxLFrip&limit=10";
 
+    //API call
     $.ajax({
         url: queryURL,
         method: "GET"
     })
         .done(function (response) {
-            for (var i = 0; i < response.data.length; i++) {
 
+            //for loop generating
+            for (var i = 0; i < response.data.length; i++) {
+               
+                // local variables
                 var searchDiv = $('<div class="search-item">');
                 var rating = response.data[i].rating;
                 var p = $('<p>').text('Rating: ' + rating);
+                var image = $('<img>');
                 var animated = response.data[i].images.fixed_height.url;
                 var still = response.data[i].images.fixed_height_still.url;
-                var image = $('<img>');
+                // adding attributes to the image
                 image.attr('src', still);
                 image.attr('data-still', still);
                 image.attr('data-animated', animated);
                 image.attr('data-state', 'still');
+                // adding class to image
                 image.addClass('searchImage');
-                // searchDiv.append(p);
+                // appending image with classes, attr to searchDiv
                 searchDiv.append(image,p);
+                //linking searchDiv  html page
                 $('#searches').append(searchDiv);
-
-
             }
 
         })
 })
 
+//onclick to aniname image from still to animated
 $(document).on('click', '.searchImage', function () {
     var state = $(this).attr('data-state');
     if (state === 'still') {
